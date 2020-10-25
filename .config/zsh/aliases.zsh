315f1f523a79c0c4feb938ae537e2e97bf74d515
Paul Owen <paul@noreply.users.github.com>
2020-10-25 12:24:46 +0000

    Add additional aliases for WSL Remove/fixup couple other aliases

--- .config/zsh/aliases.zsh
+++ .config/zsh/aliases.zsh
@@ -23,29 +23,27 @@ alias cat='batcat'
 
 # WSL aliases for common apps
 if (( ${+WSLENV} )); then
+    [ -s /mnt/c/Windows/System32/OpenSSH/ssh.exe ] && export GIT_SSH="ssh.exe" 
     [ -s /mnt/c/Windows/System32/OpenSSH/ssh.exe ] && alias ssh='/mnt/c/Windows/System32/OpenSSH/ssh.exe'
     [ -s /mnt/c/Windows/System32/OpenSSH/ssh-keygen.exe ] && alias ssh-keygen='/mnt/c/Windows/System32/OpenSSH/ssh-keygen.exe'
     [ -s /mnt/c/Windows/System32/OpenSSH/ssh-add.exe ] && alias ssh-add='/mnt/c/Windows/System32/OpenSSH/ssh-add.exe'
+    [ -s /mnt/c/Windows/System32/OpenSSH/scp.exe ] && alias scp='/mnt/c/Windows/System32/OpenSSH/scp.exe'
+    [ -s /mnt/c/Windows/System32/OpenSSH/sftp.exe ] && alias sftp='/mnt/c/Windows/System32/OpenSSH/sftp.exe'
     [ -s /mnt/c/Program\ Files\ \(x86\)/gnupg/bin/gpg.exe ] && alias gpg='/mnt/c/Program\ Files\ \(x86\)/gnupg/bin/gpg.exe'
-    [ -s /mnt/c/Windows/System32/OpenSSH/ssh.exe ] && export GIT_SSH="ssh.exe" 
 fi
+
+# Handle .dotfiles in $HOME
 alias shconfig='git --git-dir=$HOME/.dotfiles --work-tree=$HOME'
 
 # Suffix aliases
 alias -s {yml,yaml,json,conf,ini,zsh}=vim
 
-# Global aliases
-alias -g G='| grep -i'
-
-# Handle my dotfiles in $HOME
-alias git-dot='git --git-dir="$HOME"/.dotfiles/ --work-tree="$HOME"'
-
 # fzf
 alias fge='fzf_grep_edit'
 alias ffe='fzf_find_edit'
-alias gll='fzf_git_log'
-alias grl='fzf_git_reflog'
-alias glS='fzf_git_log_pickaxe'
 
 # apt
 alias asi='apt-cache pkgnames | fzf --header "[apt:install]" | xargs -d " " -- sudo apt-get install'
+
+# Global aliases
+alias -g G='| grep -i'
