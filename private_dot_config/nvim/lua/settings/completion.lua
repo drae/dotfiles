338f78f4ae0679cd2cad62afc61d952c3ae47331
Paul Owen <drae@users.noreply.github.com>
2022-10-02 21:13:53 +0100

    Absentia told me to delete these lines Totally blaming him if it breaks stuff!

--- private_dot_config/nvim/lua/settings/completion.lua
+++ private_dot_config/nvim/lua/settings/completion.lua
@@ -34,8 +34,6 @@ cmp.setup({
     ['<Tab>'] = cmp.mapping(function(fallback)
       if cmp.visible() then
         cmp.select_next_item()
-      elseif luasnip.expand_or_jumpable() then
-        luasnip.expand_or_jump()
       else
         fallback()
       end
@@ -43,8 +41,6 @@ cmp.setup({
     ['<S-Tab>'] = cmp.mapping(function(fallback)
       if cmp.visible() then
         cmp.select_prev_item()
-      elseif luasnip.jumpable(-1) then
-        luasnip.jump(-1)
       else
         fallback()
       end
