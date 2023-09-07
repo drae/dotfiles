c924ecb8a061196c8201e0fcd552585702279288
Paul Owen <drae@users.noreply.github.com>
2023-09-07 15:30:36 +0100

    cope with multiple shell locations for binary

--- home/.chezmoiscripts/run_onchange_before_30-configure-zsh-shell.sh
+++ home/.chezmoiscripts/run_onchange_before_30-configure-zsh-shell.sh
@@ -26,7 +26,7 @@ function install_zsh_shell() {
     fi
 
     if ! is_zsh_current_shell; then
-      zsh=$(grep $(command -v zsh) /etc/shells | tr -d '\n')
+      zsh=$(grep $(command -v zsh) /etc/shells | head -1)
       echo "Changing shell to zsh"
       chsh -s "${zsh}"
     fi
