eced74f715da17f5e45b242aec4d89745fd7fb10
Paul Owen <drae@users.noreply.github.com>
2023-09-20 17:48:23 +0100

    update: switch to lean prompt in zsh update: various fish shell updates

--- home/private_dot_config/private_fish/conf.d/abbr.fish
+++ home/private_dot_config/private_fish/conf.d/abbr.fish
@@ -1,6 +1,14 @@
-# abbr -a k kubectl
-# abbr -a kc kubecolor
+abbr df 'df -H'
+abbr diskspace 'sudo df -h | grep -E "sd|lv|Size"'
+abbr du 'du -ch'
+abbr meminfo 'free -m -l -t'
+abbr gpumeminfo 'grep -i --color memory /var/log/Xorg.0.log'
+abbr cpuinfo lscpu
+abbr pscpu 'ps auxf | sort -nr -k 3'
+abbr pscpu10 'ps auxf | sort -nr -k 3 | head -10'
+abbr psmem 'ps auxf | sort -nr -k 4'
+abbr psmem10 'ps auxf | sort -nr -k 4 | head -10'
 
 if command -sq bat
-  abbr -a cat bat
+    abbr -a cat bat
 end
