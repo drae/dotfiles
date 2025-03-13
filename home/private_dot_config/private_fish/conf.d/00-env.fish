eede2ad9bd850bbb8dea6f731aa792bb8b5a2245
Paul Owen <drae@users.noreply.github.com>
2025-03-13 18:24:01 +0000

    Add additional locale env vars - quiet some app errors

--- home/private_dot_config/private_fish/conf.d/00-env.fish
+++ home/private_dot_config/private_fish/conf.d/00-env.fish
@@ -2,6 +2,8 @@ set -gx XDG_CONFIG_HOME $HOME/.config
 set -gx XDG_DATA_HOME $HOME/.local/share
 
 set -gx LANG "en_GB.utf-8"
+set -gx LC_CTYPE "en_GB.utf-8"
+set -gx LC_ALL "en_GB.utf-8"
 
 set -gx VISUAL nvim
 set -gx EDITOR nvim
