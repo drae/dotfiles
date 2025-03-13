6c9904a5e2d0ea2e43237ab8288af44efceb299c
Paul Owen <drae@users.noreply.github.com>
2025-03-13 00:42:10 +0000

    TIdy up move to fish

--- home/private_dot_config/private_fish/conf.d/abbr.fish
+++ home/private_dot_config/private_fish/conf.d/abbr.fish
@@ -1,15 +1,17 @@
-if type -q kubectl
-  abbr k kubectl
+if type -q kubecolor
+    abbr k kubecolor
+else if type -q kubectl
+    abbr k kubectl
 end
 
 if type -q kubecm
-  abbr kc kubecm
+    abbr kc kubecm
 end
 
 if type -q doggo
-  abbr dig doggo
+    abbr dig doggo
 end
 
 if type -q bat
-  abbr cat bat
+    abbr cat bat
 end
--- home/private_dot_config/private_fish/conf.d/krew.fish
+++ home/private_dot_config/private_fish/conf.d/krew.fish
@@ -1,4 +1,4 @@
-if type -q kubectl-krew
-  abbr krew kubectl krew
-  fish_add_path --global $HOME/.krew/bin
+if type -q krew
+    abbr krew kubectl krew
+    fish_add_path --global $HOME/.krew/bin
 end
