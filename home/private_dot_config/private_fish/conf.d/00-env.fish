294ad6f4ecc2b294b7744c35a3d3279939e91b36
Paul Owen <drae@users.noreply.github.com>
2025-04-08 00:56:11 +0100

    fix(env): standardize locale settings to use uppercase UTF-8
    Signed-off-by: Paul Owen <drae@users.noreply.github.com>

--- home/private_dot_config/private_fish/conf.d/00-env.fish
+++ home/private_dot_config/private_fish/conf.d/00-env.fish
@@ -1,9 +1,8 @@
 set -gx XDG_CONFIG_HOME $HOME/.config
 set -gx XDG_DATA_HOME $HOME/.local/share
 
-set -gx LANG "en_GB.utf-8"
-set -gx LC_CTYPE "en_GB.utf-8"
-set -gx LC_ALL "en_GB.utf-8"
+set -gx LANG "en_GB.UTF-8"
+set -gx LC_ALL "en_GB.UTF-8"
 
 set -gx VISUAL nvim
 set -gx EDITOR nvim
