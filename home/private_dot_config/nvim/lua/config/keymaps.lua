03f258089389631df68e9f76fe1fb893308107f2
Paul Owen <drae@users.noreply.github.com>
2025-08-11 23:51:36 +0100

    Remove mapping for formatting from neovim
    Signed-off-by: Paul Owen <drae@users.noreply.github.com>

--- home/private_dot_config/nvim/lua/config/keymaps.lua
+++ home/private_dot_config/nvim/lua/config/keymaps.lua
@@ -2,17 +2,10 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
-local vim, LazyVim = _G.vim, _G.LazyVim
+local vim = _G.vim
 local map = vim.keymap.set
 
 -- quit
 map("n", "<leader>qq", "<cmd>qa<cr>", {
     desc = "Quit All",
-})
-
--- formatting
-map({ "n", "v" }, "<leader>cf", function()
-    LazyVim.format({ force = true })
-end, {
-    desc = "Format",
 })
