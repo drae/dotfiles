762b0ee1f2efd72463f96d0bcff0e8584fe20241
Paul Owen <drae@users.noreply.github.com>
2025-08-12 16:36:21 +0100

    Rationalise mason lsp/formatters/etc.
    Signed-off-by: Paul Owen <drae@users.noreply.github.com>

--- home/private_dot_config/nvim/lua/plugins/lsp.lua
+++ home/private_dot_config/nvim/lua/plugins/lsp.lua
@@ -4,15 +4,25 @@ return {
     "williamboman/mason.nvim",
     opts = {
         ensure_installed = {
-            "ansible-lint",
+            -- ansible
             "ansible-language-server",
+            "ansible-lint",
+            -- astro
             "astro-language-server",
+            -- lua
             "lua-language-server",
-            "markdownlint-cli2",
-            "prettierd",
-            "shellcheck",
             "stylua",
+            -- markdown
+            "markdownlint-cli2",
+            -- python
+            "pyright",
+            "ruff",
+            -- shell
+            "shellcheck",
+            "shfmt",
+            -- typescript/javascript (html/css/web/etc.)
             "typescript-language-server",
+            "prettierd",
         },
     },
 }
