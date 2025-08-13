08f3e0fa9c10e84657415f432078bbc43eb6e5ac
Paul Owen <drae@users.noreply.github.com>
2025-08-13 23:36:50 +0100

    refactor(lualine): simplify opts configuration by removing unnecessary function
    Signed-off-by: Paul Owen <drae@users.noreply.github.com>

--- home/private_dot_config/nvim/lua/plugins/lualine.lua
+++ home/private_dot_config/nvim/lua/plugins/lualine.lua
@@ -1,10 +1,7 @@
 return {
     "nvim-lualine/lualine.nvim",
     dependencies = { "nvim-tree/nvim-web-devicons" },
-    -- override LazyVim configuration
-    opts = function()
-        return {
-            theme = "gruvbox-material",
-        }
-    end,
+    opts = {
+        theme = "gruvbox-material",
+    },
 }
